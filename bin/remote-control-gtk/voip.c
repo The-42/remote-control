2f8d31dff000dc60d458387a7bb4fb2aa060b82a
Soeren Grunewald <soeren.grunewald@avionic-design.de>
2010-12-14 11:25:35 +0100

    remote-control-gtk: Implemented missing functions.
    There were several signal handler functions not implemented yet.
    There was also and issue with user data handling. Which has been fixed by
    using a different property name.

--- bin/remote-control-gtk/voip.c
+++ bin/remote-control-gtk/voip.c
@@ -1,9 +1,13 @@
 #include "medcom-client-gtk.h"
 #include "voip.h"
 
-#define USER_DATA_KEY "user"
+#ifndef MAX_URI
+#define MAX_URI       1024
+#endif
+#define USER_DATA_KEY "d7zsfiac3"
 
 struct panel_data {
+	struct medcom_client *client;
 	/* connection */
 	GtkEntry *server_address;
 	GtkEntry *server_port;
@@ -25,10 +29,11 @@ static void voip_panel_dump(struct panel_data *pnl)
 	fprintf(stdout, "   call_number:       %p\n", pnl->call_number);
 }
 
+/*
 static void voip_panel_destroy_userdata(gpointer data)
 {
 	fprintf(stdout, "%s: data: %p\n", __func__, data);
-}
+}*/
 
 int voip_panel_append(GtkWidget *widget, const gchar* to_append)
 {
@@ -115,32 +120,115 @@ void on_voip_server_button_login_clicked(GtkWidget *widget, gpointer data)
 	login.username = g_strdup(gtk_entry_get_text(pnl->server_username));
 	login.password = g_strdup(gtk_entry_get_text(pnl->server_password));
 
-	ret = medcom_voip_login(g_client, &login);
+	ret = medcom_voip_login(pnl->client, &login);
 	if (ret < 0) {
 		fprintf(stderr, "login failed %d\n", ret);
 		goto out;
 	}
 
 out:
-//	g_free(login.server);
-//	g_free(login.username);
-//	g_free(login.password);
 	fprintf(stdout, "< %s()\n", __func__);
 }
 
 void on_voip_server_button_logout_clicked(GtkWidget *widget, gpointer data)
 {
-	fprintf(stdout, "%s\n", __func__);
+	struct panel_data *pnl = NULL;
+	int32_t ret;
+
+	fprintf(stdout, "> %s(widget=%p, data=%p)\n", __func__, widget, data);
+
+	if (!widget) {
+		fprintf(stderr, " widget not valid\n");
+		goto out;
+	}
+
+	pnl = (struct panel_data*)g_object_get_data(G_OBJECT(widget), USER_DATA_KEY);
+	if (!pnl) {
+		fprintf(stderr, " panel not valid\n");
+		goto out;
+	}
+
+	ret = medcom_voip_logout(pnl->client);
+	if (ret < 0) {
+		fprintf(stderr, "logout failed %d\n", ret);
+		goto out;
+	}
+
+out:
+	fprintf(stdout, "< %s()\n", __func__);
 }
 
 void on_voip_button_hook_clicked(GtkWidget *widget, gpointer data)
 {
-	fprintf(stdout, "%s\n", __func__);
+	struct panel_data *pnl = NULL;
+	const gchar *uri = NULL;
+	int32_t ret;
+
+	fprintf(stdout, "> %s(widget=%p, data=%p)\n", __func__, widget, data);
+
+	if (!widget) {
+		fprintf(stderr, " widget not valid\n");
+		goto out;
+	}
+
+	pnl = (struct panel_data*)g_object_get_data(G_OBJECT(widget), USER_DATA_KEY);
+	if (!pnl) {
+		fprintf(stderr, " panel not valid\n");
+		goto out;
+	}
+
+	if (gtk_entry_get_text_length(GTK_ENTRY(pnl->call_number)) != 0)
+		uri = gtk_entry_get_text(GTK_ENTRY(pnl->call_number));
+
+	/* depending on uri we accept or make a call */
+	if (uri == NULL) {
+		gchar *caller = NULL;
+
+		ret = medcom_voip_accept_incoming(pnl->client, &caller);
+		/* update caller info */
+		gtk_entry_set_text(GTK_ENTRY(pnl->call_number), caller);
+
+		if (caller)
+			free(caller);
+	}
+	else
+		ret = medcom_voip_connect_to(pnl->client, uri);
+
+	if (ret < 0) {
+		fprintf(stderr, "logout failed %d\n", ret);
+		goto out;
+	}
+
+out:
+	fprintf(stdout, "< %s()\n", __func__);
 }
 
 void on_voip_button_unhook_clicked(GtkWidget *widget, gpointer data)
 {
-	fprintf(stdout, "%s\n", __func__);
+	struct panel_data *pnl = NULL;
+	int32_t ret;
+
+	fprintf(stdout, "> %s(widget=%p, data=%p)\n", __func__, widget, data);
+
+	if (!widget) {
+		fprintf(stderr, " widget not valid\n");
+		goto out;
+	}
+
+	pnl = (struct panel_data*)g_object_get_data(G_OBJECT(widget), USER_DATA_KEY);
+	if (!pnl) {
+		fprintf(stderr, " panel not valid\n");
+		goto out;
+	}
+
+	ret = medcom_voip_disconnect(pnl->client);
+	if (ret < 0) {
+		fprintf(stderr, "disconnect failed %d\n", ret);
+		goto out;
+	}
+
+out:
+	fprintf(stdout, "< %s()\n", __func__);
 }
 
 static int voip_panel_create(struct panel *panel, GtkWidget **widget)
@@ -168,6 +256,7 @@ static int voip_panel_create(struct panel *panel, GtkWidget **widget)
 	priv = malloc(sizeof(struct panel_data));
 	if (priv) {
 		memset(priv, 0, sizeof(*priv));
+		priv->client = g_client;
 
 		priv->server_address    = GTK_ENTRY(glade_xml_get_widget(xml, "voip_connection_server_addr_entry"));
 		priv->server_port       = GTK_ENTRY(glade_xml_get_widget(xml, "voip_connection_server_port_entry"));
@@ -181,7 +270,9 @@ static int voip_panel_create(struct panel *panel, GtkWidget **widget)
 
 		fprintf(stdout, "voip_panel=%p\n", container);
 		voip_panel_dump(priv);
-		g_object_set_data_full(G_OBJECT(container), USER_DATA_KEY, priv, voip_panel_destroy_userdata);
+
+		g_object_set_data(G_OBJECT(priv->call_number), USER_DATA_KEY, priv);
+		g_object_set_data(G_OBJECT(container), USER_DATA_KEY, priv);
 	}
 
 	*widget = container;
