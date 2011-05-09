eae69d348746196dbb15ff9118d1834b6f7ae8dd
Thierry Reding <thierry.reding@avionic-design.de>
2011-05-09 15:06:23 +0200

    server: Make backlight backend selectable.
    This commit also adds a null backend to serve as a fallback if no actual
    support is requested.

---
+++ src/server/backlight-null.c
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2011 Avionic Design GmbH
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif
+
+#include "remote-control-stub.h"
+#include "remote-control.h"
+
+int backlight_create(struct backlight **backlightp)
+{
+	return 0;
+}
+
+int backlight_free(struct backlight *backlight)
+{
+	return 0;
+}
+
+int backlight_enable(struct backlight *backlight, bool enable)
+{
+	return -ENOSYS;
+}
+
+int backlight_set(struct backlight *backlight, unsigned int brightness)
+{
+	return -ENOSYS;
+}
+
+int backlight_get(struct backlight *backlight)
+{
+	return -ENOSYS;
+}
