2cce47aa53353c304df350da480bf73ce430a5f4
Hendrik Lippek <hendrik.lippek@avionic-design.de>
2011-10-14 17:43:05 +0200

    Increase the size of the toolbar elements.

--- bin/remote-control-browser/webkit-browser.c
+++ bin/remote-control-browser/webkit-browser.c
@@ -211,6 +211,7 @@ static void webkit_browser_init(WebKitBrowser *browser)
 	toolbar = gtk_toolbar_new();
 
 	gtk_toolbar_set_style(GTK_TOOLBAR(toolbar), GTK_TOOLBAR_ICONS);
+	gtk_toolbar_set_icon_size(GTK_TOOLBAR(toolbar), GTK_ICON_SIZE_DIALOG);
 
 	item = gtk_tool_button_new_from_stock(GTK_STOCK_GO_BACK);
 	g_signal_connect(G_OBJECT(item), "clicked",
