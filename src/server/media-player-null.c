ccd5ebb59ede0d94d6104a2e5ab6746fd604204b
Thierry Reding <thierry.reding@avionic-design.de>
2012-01-19 12:04:31 +0100

    server: media-player-null: Make backend consistent.
    This commit makes the dummy media-player backend more consistent with
    other dummy backends. No memory is allocated and all operations, except
    for initialization and teardown return -ENOSYS.

--- src/server/media-player-null.c
+++ src/server/media-player-null.c
@@ -15,34 +15,13 @@
 #include "remote-control-stub.h"
 #include "remote-control.h"
 
-struct media_player {
-	enum media_player_state state;
-};
-
 int media_player_create(struct media_player **playerp)
 {
-	struct media_player *player;
-
-	if (!playerp)
-		return -EINVAL;
-
-	player = malloc(sizeof(*player));
-	if (!player)
-		return -ENOMEM;
-
-	memset(player, 0, sizeof(*player));
-	player->state = MEDIA_PLAYER_STOPPED;
-
-	*playerp = player;
 	return 0;
 }
 
 int media_player_free(struct media_player *player)
 {
-	if (!player)
-		return -EINVAL;
-
-	free(player);
 	return 0;
 }
 
@@ -50,88 +29,59 @@ int media_player_set_output_window(struct media_player *player,
 		unsigned int x, unsigned int y, unsigned int width,
 		unsigned int height)
 {
-	if (!player)
-		return -EINVAL;
-
 	return -ENOSYS;
 }
 
 int media_player_set_uri(struct media_player *player, const char *uri)
 {
-	if (!player || !uri)
-		return -EINVAL;
-
 	return -ENOSYS;
 }
 
 int media_player_get_uri(struct media_player *player, char **urip)
 {
-	if (!player || !urip)
-		return -EINVAL;
-
 	return -ENOSYS;
 }
 
 int media_player_play(struct media_player *player)
 {
-	if (!player)
-		return -EINVAL;
-
 	return -ENOSYS;
 }
 
 int media_player_stop(struct media_player *player)
 {
-	if (!player)
-		return -EINVAL;
-
 	return -ENOSYS;
 }
 
 int media_player_pause(struct media_player *player)
 {
-	g_return_val_if_fail(player != NULL, -EINVAL);
-
 	return -ENOSYS;
 }
 
 int media_player_resume(struct media_player *player)
 {
-	g_return_val_if_fail(player != NULL, -EINVAL);
-
 	return -ENOSYS;
 }
 
 int media_player_get_duration(struct media_player *player,
 		unsigned long *duration)
 {
-	g_return_val_if_fail(player != NULL, -EINVAL);
-
 	return -ENOSYS;
 }
 
 int media_player_get_position(struct media_player *player,
 		unsigned long *position)
 {
-	g_return_val_if_fail(player != NULL, -EINVAL);
-
 	return -ENOSYS;
 }
 
 int media_player_set_position(struct media_player *player,
 		unsigned long position)
 {
-	g_return_val_if_fail(player != NULL, -EINVAL);
-
 	return -ENOSYS;
 }
 
 int media_player_get_state(struct media_player *player,
 		enum media_player_state *statep)
 {
-	if (!player || !statep)
-		return -EINVAL;
-
-	*statep = player->state;
-	return 0;
+	return -ENOSYS;
 }
