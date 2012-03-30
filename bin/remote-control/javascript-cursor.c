93071073b7f4c07d7ded773e2501430912e84e2f
Soeren Grunewald <soeren.grunewald@avionic-design.de>
2012-03-30 19:45:57 +0200

    bin/remote-control: Use uinput for cursor movement.
    To guerantee that the cursor can be moved we need our own input device. Therfor
    we create a virtual touch device using uinput.
    Warping the cursor around using gdk functions does only work in our own
    window (and is left in as fallback in case uinput is not available).

--- bin/remote-control/javascript-cursor.c
+++ bin/remote-control/javascript-cursor.c
@@ -12,16 +12,22 @@
 
 #include <errno.h>
 #include <fcntl.h>
+#include <string.h>
 #include <unistd.h>
 
+#include <linux/uinput.h>
 #include <gtk/gtk.h>
 
 #include "javascript.h"
+
+#define KEY_PRESSED 1
+#define KEY_RELEASE 0
 
 struct cursor {
 	GdkDisplay *display;
 	GdkScreen *screen;
 	RemoteControlWebkitWindow *window;
+	int uinput; /* dummy input device */
 };
 
 static void set_exception_text(JSContextRef context,JSValueRef *exception,
@@ -32,6 +38,41 @@ static void set_exception_text(JSContextRef context,JSValueRef *exception,
 		*exception = JSValueMakeString(context, text);
 		JSStringRelease(text);
 	}
+}
+
+static inline int uinput_send_event(int uinput, int type, int code, int value)
+{
+	struct input_event event;
+	int ret;
+
+	memset(&event, 0, sizeof(event));
+	event.type  = type;
+	event.code  = code;
+	event.value = value;
+
+	ret = write(uinput, &event, sizeof(event));
+	if (ret != sizeof(event)) {
+		g_warning("%s: failed to send event %s",
+		          __func__, g_strerror(errno));
+		return -1;
+	}
+
+	return 0;
+}
+
+static void cursor_uinput_move(struct cursor *priv, int x, int y)
+{
+	uinput_send_event(priv->uinput, EV_ABS, ABS_X, x);
+	uinput_send_event(priv->uinput, EV_ABS, ABS_Y, y);
+	uinput_send_event(priv->uinput, EV_SYN, SYN_REPORT, 0);
+}
+
+static void cursor_uinput_click(struct cursor *priv)
+{
+	uinput_send_event(priv->uinput, EV_KEY, BTN_TOUCH, KEY_PRESSED);
+	uinput_send_event(priv->uinput, EV_SYN, SYN_REPORT, 0);
+	uinput_send_event(priv->uinput, EV_KEY, BTN_TOUCH, KEY_RELEASE);
+	uinput_send_event(priv->uinput, EV_SYN, SYN_REPORT, 0);
 }
 
 static JSValueRef cursor_moveto_callback(JSContextRef context,
@@ -69,11 +110,14 @@ static JSValueRef cursor_moveto_callback(JSContextRef context,
 	x = JSValueToNumber(context, argv[0], exception);
 	y = JSValueToNumber(context, argv[1], exception);
 
-	g_assert(priv->display != NULL);
-	g_assert(priv->screen != NULL);
-
-	gdk_display_warp_pointer(priv->display, priv->screen, x, y);
-	gdk_display_flush(priv->display);
+	if (priv->uinput) {
+		cursor_uinput_move(priv, x, y);
+	} else {
+		g_assert(priv->display != NULL);
+		g_assert(priv->screen != NULL);
+		gdk_display_warp_pointer(priv->display, priv->screen, x, y);
+		gdk_display_flush(priv->display);
+	}
 
 	return JSValueMakeBoolean(context, TRUE);
 }
@@ -85,6 +129,7 @@ static JSValueRef cursor_clickat_callback(JSContextRef context,
                                           JSValueRef *exception)
 {
 	struct cursor *priv = JSObjectGetPrivate(object);
+	GdkWindow *window;
 	int x, y;
 
 	if (!priv) {
@@ -113,12 +158,19 @@ static JSValueRef cursor_clickat_callback(JSContextRef context,
 	x = JSValueToNumber(context, argv[0], exception);
 	y = JSValueToNumber(context, argv[1], exception);
 
-	g_assert(priv->window != NULL);
+	if (priv->uinput) {
+		cursor_uinput_move(priv, x, y);
+		cursor_uinput_click(priv);
+	} else {
+		g_assert(priv->window != NULL);
+		window =  gtk_widget_get_window(GTK_WIDGET(GTK_WINDOW(priv->window)));
+		g_assert(window != NULL);
 
-	gdk_test_simulate_button(priv->window, x, y, 1, GDK_BUTTON1_MASK,
-	                         GDK_BUTTON_PRESS);
-	gdk_test_simulate_button(priv->window, x, y, 1, GDK_BUTTON1_MASK,
-	                         GDK_BUTTON_RELEASE);
+		gdk_test_simulate_button(window, x, y, 1, GDK_BUTTON1_MASK,
+		                         GDK_BUTTON_PRESS);
+		gdk_test_simulate_button(window, x, y, 1, GDK_BUTTON1_MASK,
+		                         GDK_BUTTON_RELEASE);
+	}
 
 	return JSValueMakeBoolean(context, TRUE);
 }
@@ -210,16 +262,118 @@ static const JSStaticValue cursor_properties[] = {
 	}
 };
 
+/**
+ * Create a dummy input device. This is needed to make cursor movement work
+ * if no input device (like mouse) is attached.
+ */
+static int cursor_uinput_create(struct cursor *priv)
+{
+	static const char *DEVS[] = {
+		"/dev/uinput",
+		"/dev/input/uinput"
+	};
+	struct uinput_user_dev dev;
+	gint width, height;
+	int err, fd, i;
+
+	for (i=0; i<G_N_ELEMENTS(DEVS); i++) {
+		fd = open(DEVS[i], O_WRONLY|O_NDELAY|O_CLOEXEC);
+		if (fd < 0) {
+			g_debug("%s: unable to open %s", __func__, DEVS[i]);
+			continue;
+		}
+		break;
+	}
+	if (fd < 0) {
+		g_warning("%s: no uinput device found, try: modprobe uinput",
+		          __func__);
+		return -ENOENT;
+	}
+
+	fcntl (fd, F_SETFD, FD_CLOEXEC);
+
+	width = gdk_screen_get_width(priv->screen) - 1;
+	height = gdk_screen_get_height(priv->screen) - 1;
+
+	memset(&dev, 0, sizeof(dev));
+	strncpy(dev.name, "Avionic Design GmbH virtual touch", UINPUT_MAX_NAME_SIZE);
+	dev.id.bustype = BUS_VIRTUAL;
+	dev.absmax[ABS_X] = width;
+	dev.absmax[ABS_Y] = height;
+	dev.absmax[ABS_MT_POSITION_X] = width;
+	dev.absmax[ABS_MT_POSITION_Y] = height;
+	dev.absmax[ABS_MT_TOUCH_MAJOR] = 1;
+
+	/* setting up device info */
+	err = write(fd, &dev, sizeof(dev));
+	if (err != sizeof(dev)) {
+		g_warning("%s: setup error, write failed with %s",
+		          __func__, g_strerror(-err));
+		goto cleanup;
+	}
+
+	err = ioctl (fd, UI_SET_PHYS, PACKAGE_NAME"/"PACKAGE_VERSION);
+
+	if (ioctl(fd, UI_SET_EVBIT, EV_KEY) ||
+	    ioctl(fd, UI_SET_KEYBIT, BTN_TOUCH) ||
+	    ioctl(fd, UI_SET_EVBIT, EV_ABS) ||
+	    ioctl(fd, UI_SET_ABSBIT, ABS_X) ||
+	    ioctl(fd, UI_SET_ABSBIT, ABS_Y))
+	{
+		g_warning("%s: setup error, ioctrl failed %s",
+		          __func__, g_strerror(errno));
+		goto cleanup;
+	}
+
+	/* "creating device; DO NOT FORGET UNREGISTER */
+	err = ioctl(fd, UI_DEV_CREATE);
+	if (err < 0) {
+		g_warning("%s: create uinput device failed %s",
+		          __func__, g_strerror(-err));
+		goto cleanup;
+	}
+
+	priv->uinput = fd;
+	return 0;
+
+cleanup:
+	if (fd)
+		close(fd);
+
+	return -ENOENT;
+}
+
+static void cursor_uinput_destroy(struct cursor *priv)
+{
+	int err;
+
+	if (!priv->uinput)
+		return;
+
+	err = ioctl(priv->uinput, UI_DEV_DESTROY);
+	if (err < 0) {
+		g_warning("%s: create uinput device failed %s",
+		          __func__, g_strerror(-err));
+	}
+
+	close(priv->uinput);
+	priv->uinput = 0;
+}
+
 static void cursor_initialize(JSContextRef context, JSObjectRef object)
 {
+	struct cursor *priv = JSObjectGetPrivate(object);
+
+	cursor_uinput_create(priv);
 }
 
 static void cursor_finalize(JSObjectRef object)
 {
 	struct cursor *priv = JSObjectGetPrivate(object);
 
-	g_free(priv);
+	cursor_uinput_destroy(priv);
 	JSObjectSetPrivate(object, NULL);
+	g_free(priv);
 }
 
 static const JSClassDefinition cursor_classdef = {
