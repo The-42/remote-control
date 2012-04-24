The-42/remote-control

commit 154b3e8f646f38133562bfcfc4041348a7890904

media-player-gtk-gst: Restructured audio language selection code.
This will restructure the code for audio language selection in order to
reduce the indention depth.

src/server/media-player-gtk-gst.c
@@ -161,40 +161,40 @@ static void player_check_audio_tracks(GstElement *playbin,
                                       gpointer user_data)
 {
 	struct media_player *player = (struct media_player*)user_data;
+	gchar *language_code = NULL;
 	GstTagList *taglist = NULL;
 	int track_priority = -1;
 	int select_track = -1;
 	guint n_audio;
+	int priority;
 	int i;
 
 	g_object_get(G_OBJECT(playbin), "n-audio", &n_audio, NULL);
-
+	track_priority = n_audio - 1;
 	for (i = 0; i < n_audio; i++) {
 		g_signal_emit_by_name(playbin, "get-audio-tags", i, &taglist);
+		if (!taglist)
+			continue;
 
-		if (taglist) {
-			gchar *language_code = NULL;
-			int priority;
+		gst_structure_get (GST_STRUCTURE(taglist),
+		                   "language-code", G_TYPE_STRING,
+		                   &language_code, NULL);
 
-			gst_structure_get (GST_STRUCTURE(taglist), "language-code",
-							   G_TYPE_STRING, &language_code, NULL);
-			priority =
-					player_get_language_code_priority(player, language_code);
-
-			if (priority > -1 && (track_priority > priority ||
-								  track_priority == -1)) {
-				track_priority = priority;
-				select_track = i;
-			}
-
-			gst_tag_list_free(taglist);
-			g_free(language_code);
+		priority = player_get_language_code_priority(player,
+		                                             language_code);
+		if (-1 < priority && priority < track_priority) {
+			track_priority = priority;
+			select_track = i;
 		}
+
+		gst_tag_list_free(taglist);
+		g_free(language_code);
 	}
 
 	if (select_track != -1) {
-		g_print("Select audio track %d\n", select_track);
-		g_object_set(G_OBJECT(playbin), "current-audio", select_track, NULL);
+		g_debug("Select audio track %d", select_track);
+		g_object_set(G_OBJECT(playbin), "current-audio",
+		             select_track, NULL);
 	}
 }
 

commit c48c1dc9f499e842b442d1f9e18d1d9c9e4fbdad

media-player-gtk-gst: Fix audio selection code.
Commit 154b3e8 introduced a regression in the selection of preferred
languages. The track_priority is set to INT_MAX at the beginning now, so each
regular priority will be lower, hence the first preferred track will be
selected.

src/server/media-player-gtk-gst.c
@@ -161,16 +161,15 @@ static void player_check_audio_tracks(GstElement *playbin,
                                       gpointer user_data)
 {
 	struct media_player *player = (struct media_player*)user_data;
+	int track_priority = INT_MAX;
 	gchar *language_code = NULL;
 	GstTagList *taglist = NULL;
-	int track_priority = -1;
 	int select_track = -1;
 	guint n_audio;
 	int priority;
 	int i;
 
 	g_object_get(G_OBJECT(playbin), "n-audio", &n_audio, NULL);
-	track_priority = n_audio - 1;
 	for (i = 0; i < n_audio; i++) {
 		g_signal_emit_by_name(playbin, "get-audio-tags", i, &taglist);
 		if (!taglist)