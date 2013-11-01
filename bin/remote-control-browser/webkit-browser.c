000fa183690f130f085e2b1a78a9b43565a7e72e
Hendrik Lippek <hendrik.lippek@avionic-design.de>
2013-11-01 15:21:25 +0100

    browser: Update the year in copyright info.
    Change-Id: I8264bf63a1176effd36cea359b302ed401f2f37f
    Signed-off-by: Hendrik Lippek <hendrik.lippek@avionic-design.de>
    Reviewed-on: http://review.adnet.avionic-design.de/583
    Reviewed-by: Soeren Grunewald <soeren.grunewald@avionic-design.de>

--- bin/remote-control-browser/webkit-browser.c
+++ bin/remote-control-browser/webkit-browser.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011-2012 Avionic Design GmbH
+ * Copyright (C) 2011-2013 Avionic Design GmbH
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
