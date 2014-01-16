58c261fa8deea33e5f9e67ec254a3d35e592fd38
Bert van Hall <bert.vanhall@avionic-design.de>
2014-01-16 10:50:13 +0100

    browser: spread global hook across pre and post
    Move codec detection function to pre-global js hook. This hook is
    intended for generic and testing functions independent of websites,
    while the post-global hook is used for functions relying on a fully
    loaded DOM.
    
    Change-Id: I2944352cb40e38bcc2328ca244a2e59e1361268f
    Signed-off-by: Bert van Hall <bert.vanhall@avionic-design.de>
    Reviewed-on: http://review.adnet.avionic-design.de/995
    Reviewed-by: Julian Scheel <julian@jusst.de>

--- bin/remote-control-browser/jshooks/post-global-.js
+++ bin/remote-control-browser/jshooks/post-global-.js
@@ -10,7 +10,13 @@
 
 (function ()
 {
-	/* Not all functions in this file are enabled by default since
+	/* This file is executed after the DOM of any website is fully
+	 * loaded. Functions included here may rely on a completed DOM.
+	 * Rather generic testing or production functions not requiring
+	 * a complete DOM should be considered residing in the global
+	 * hook file pre-global-.js which is executed before this hook.
+	 *
+	 * Not all functions in this file are enabled by default since
 	 * some only matter for testing or checking once per browser
 	 * build. All functions listed can be activated by appending a
 	 * parameter tag in the url. The tag consists of the namespace
@@ -41,116 +47,15 @@
 	 */
 
 	 /* Available parameters:
-	  * 	avdglobal_list_available_codecs
 	  * 	avdglobal_detect_videos
 	  */
 
 	function init()
 	{
-		if (document.URL.indexOf('avdglobal_list_available_codecs') != -1)
-			list_available_codecs();
 		if (document.URL.indexOf('avdglobal_detect_videos') != -1) {
 			document.addEventListener('DOMNodeInserted', detect_videos, false);
 			detect_videos(null, null);
 		}
-	}
-
-	function list_available_codecs()
-	{
-		var codecs = _align_widths([
-			'application/ogg',
-			'application/ogg; codecs=bogus',
-			'application/mp4',
-			'application/mp4; codecs=bogus',
-			'application/octet-stream',
-			'application/octet-stream; codecs=bogus',
-			'audio/3gpp',
-			'audio/3gpp2',
-			'audio/aac',
-			'audio/x-aac',
-			'audio/aiff',
-			'audio/x-aiff',
-			'audio/ac3',
-			'audio/x-ac3',
-			'audio/basic',
-			'audio/flac',
-			'audio/x-flac',
-			'audio/mid',
-			'audio/midi',
-			'audio/x-midi',
-			'audio/mpeg',
-			'audio/x-mpeg',
-			'audio/mpegurl',
-			'audio/x-mpegurl',
-			'audio/mp4',
-			'audio/mp4; codecs=bogus',
-			'audio/ogg',
-			'audio/ogg; codecs=bogus',
-			'audio/wav',
-			'audio/wav; codecs=0',
-			'audio/wav; codecs=1',
-			'audio/wav; codecs=2',
-			'audio/wave',
-			'audio/wave; codecs=0',
-			'audio/wave; codecs=1',
-			'audio/wave; codecs=2',
-			'audio/x-wav',
-			'audio/x-wav; codecs=0',
-			'audio/x-wav; codecs=1',
-			'audio/x-wav; codecs=2',
-			'audio/x-pn-wav',
-			'audio/x-pn-wav; codecs=0',
-			'audio/x-pn-wav; codecs=1',
-			'audio/x-pn-wav; codecs=2',
-			'video/3gpp',
-			'video/3gpp2',
-			'video/avi',
-			'video/mpeg',
-			'video/x-mpeg',
-			'video/mp4',
-			'video/mp4; codecs=bogus',
-			'video/msvideo',
-			'video/x-msvideo',
-			'video/quicktime',
-			'video/ogg',
-			'video/ogg; codecs=bogus',
-			'video/mp4; codecs="avc1.42E01E, mp4a.40.2"',
-			'video/mp4; codecs="avc1.58A01E, mp4a.40.2"',
-			'video/mp4; codecs="avc1.4D401E, mp4a.40.2"',
-			'video/mp4; codecs="avc1.64001E, mp4a.40.2"',
-			'video/mp4; codecs="mp4v.20.8, mp4a.40.2"',
-			'video/mp4; codecs="mp4v.20.240, mp4a.40.2"',
-			'video/3gpp; codecs="mp4v.20.8, samr"',
-			'video/ogg; codecs="theora, vorbis"',
-			'video/ogg; codecs="theora, speex"',
-			'audio/ogg; codecs=vorbis',
-			'audio/ogg; codecs=speex',
-			'audio/ogg; codecs=flac',
-			'video/ogg; codecs="dirac, vorbis"',
-			'video/x-matroska; codecs="theora, vorbis"',
-			'audio/webm',
-			'audio/webm; codecs=vorbis',
-			'video/webm',
-			'video/webm; codecs=vorbis',
-			'video/webm; codecs=vp8',
-			'video/webm; codecs=vp8.0',
-			'video/webm; codecs="vp8, vorbis"',
-		]);
-
-		var vid = document.createElement('video');
-		var statement;
-		var str = '\n';
-
-		str += '======================================================================\n';
-		str += 'Codec support listing for this browser (probably / maybe / no):\n';
-		for (var i = 0; i < codecs.length; i++) {
-			statement = vid.canPlayType(codecs[i]);
-			if (!statement)
-				statement = 'no';
-			str += '\t' + codecs[i] + ' -- ' + statement + '\n';
-		}
-		str += '======================================================================';
-		console.log(str);
 	}
 
 	function detect_videos(evt, docroot)
@@ -239,23 +144,6 @@
 		}
 
 		console.log(logstring);
-	}
-
-	function _align_widths(arr)
-	{
-		var max_length = 0;
-
-		for (var i = 0; i < arr.length; i++) {
-			if (arr[i].length > max_length)
-				max_length = arr[i].length;
-		}
-
-		for (var i = 0; i < arr.length; i++) {
-			while (arr[i].length < max_length)
-				arr[i] += ' ';
-		}
-
-		return arr;
 	}
 
 	function _strbegins(str, substr)
