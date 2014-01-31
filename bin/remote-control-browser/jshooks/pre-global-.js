7b8f02933260e6d018d48ef590d5475c32d5dc8c
Bert van Hall <bert.vanhall@avionic-design.de>
2014-01-31 19:20:03 +0100

    browser: Append custom video element
    Generate and append a custom video element for usage by one of the
    flash-to-html5 scripts.
    
    Change-Id: I107ac1d4c5530b488e8abb878d704026df0b61d2
    Signed-off-by: Bert van Hall <bert.vanhall@avionic-design.de>
    Reviewed-on: http://review.adnet.avionic-design.de/1066
    Reviewed-by: Julian Scheel <julian@jusst.de>

--- bin/remote-control-browser/jshooks/pre-global-.js
+++ bin/remote-control-browser/jshooks/pre-global-.js
@@ -52,6 +52,11 @@
 	{
 		if (document.URL.indexOf('avdglobal_list_available_codecs') != -1)
 			list_available_codecs();
+
+		var own_vid = _makeVideoPlayer();
+		own_vid.style.display = 'None';
+		document.body.appendChild(own_vid.parentNode);
+		_insert_css();
 	}
 
 	function list_available_codecs()
@@ -150,6 +155,270 @@
 		}
 		str += '======================================================================';
 		console.log(str);
+	}
+
+	var html5_player_css =
+		'#video-container {' +
+		'	position: relative;' +
+		'	display: inline-block' +
+		'}' +
+		'#video-controls {' +
+		'	position: absolute;' +
+		'	bottom: 0;' +
+		'	left: 0;' +
+		'	right: 0;' +
+		'	padding: 10px;' +
+		'	opacity: 0;' +
+		'	white-space: nowrap' +
+		'	-webkit-transition: opacity .3s;' +
+		'	z-index: 42042;' +
+		'	background-image: -webkit-gradient(' +
+		'		linear,' +
+		'		left bottom,' +
+		'		left top,' +
+		'		color-stop(0.13, rgb(3,113,168)),' +
+		'		color-stop(1, rgb(0,136,204))' +
+		'	);' +
+		'}' +
+		'#video-controls:before {' +
+		'	vertical-align: middle;' +
+		'	display: inline-block;' +
+		'	height: 100%;' +
+		'	content: "";' +
+		'}' +
+		'#video-container:hover #video-controls { opacity: .9; }' +
+		'button, #video-time {' +
+		'	background: rgba(0,0,0,.5);' +
+		'	border: 0;' +
+		'	color: #EEE;' +
+		'	vertical-align: middle;' +
+		'	border-radius: 5px;' +
+		'	font-family: helvetica, arial;' +
+		'	font-weight: bold;' +
+		'	font-size: 120%' +
+		'	margin: 5px;' +
+		'	padding: 5px;' +
+		'}' +
+		'button:hover { cursor: pointer; }' +
+		'#video-container #seek-flexer {' +
+		'	display: block;' +
+		'	overflow: hidden;' +
+		'	padding: 5px;' +
+		'}' +
+		'#video-controls .lefthand { float: left; }' +
+		'#video-controls .righthand { float: right; }' +
+		'#video-controls input[type="range"] {' +
+		'	-webkit-appearance:none;' +
+		'	height: 6px;' +
+		'	margin: 10px;' +
+		'	background-image:-webkit-gradient(' +
+		'		linear,' +
+		'		left top,' +
+		'		right top,' +
+		'		color-stop(0, #add8e6),' +
+		'		color-stop(.5, #42abba),' +
+		'		color-stop(1, #add8e6)' +
+		'	);' +
+		'}' +
+		'#video-controls input::-webkit-slider-thumb{' +
+		'	-webkit-appearance:none;' +
+		'	width: 30px;' +
+		'	height: 30px;' +
+		'	border-radius: 30px;' +
+		'	background-image:-webkit-gradient(' +
+		'		linear,' +
+		'		left top,' +
+		'		left bottom,' +
+		'		color-stop(0, #fefefe),' +
+		'		color-stop(0.49, #dddddd),' +
+		'		color-stop(0.51, #d1d1d1),' +
+		'		color-stop(1, #a1a1a1)' +
+		'	);' +
+		'}' +
+		'#seek-bar { width: 100%; }' +
+		'#volume-bar { width: 20%; }';
+
+	/* HTML5 video element */
+	function _makeVideoPlayer()
+	{
+		function secondsToTimeString(tsecs)
+		{
+			var s = parseInt(tsecs);
+			if (!s)
+				return '00:00';
+
+			var day = Math.floor(s / (24 * 3600));
+			var hrs = Math.floor(s / 3600) % 24;
+			var min = Math.floor(s / 60) % 60;
+			var sec = s % 60;
+			var tstr = '';
+			if (day)
+				tstr += day + 'd ';
+			if (hrs)
+				tstr += (hrs < 10 ? '0' : '') + hrs + ':';
+			tstr += (min < 10 ? '0' : '') + min + ':';
+			tstr += (sec < 10 ? '0' : '') + sec;
+
+			return tstr;
+		}
+
+		var video = document.createElement('video');
+		video.id = 'avd-html5-video';
+		video.controls = '';
+		video.preload = '';
+		video.autoplay = '';
+		video.textContent = 'No can has HTML5, dude.';
+
+		var playpause = document.createElement('button');
+		playpause.id = 'play-pause';
+		playpause.className = 'play lefthand';
+		playpause.type = 'button';
+		playpause.textContent = 'Play';
+
+		var seekbar = document.createElement('input');
+		seekbar.id = 'seek-bar';
+		seekbar.type = 'range';
+		seekbar.value = 0;
+
+		var seekspan = document.createElement('span');
+		seekspan.id = 'seek-flexer';
+		seekspan.appendChild(seekbar);
+
+		var videotime = document.createElement('span');
+		videotime.id = 'video-time';
+		videotime.className = 'righthand';
+		videotime.appendChild(document.createTextNode('- / -'));
+
+		var mute = document.createElement('button');
+		mute.id = 'mute';
+		mute.className = 'righthand';
+		mute.type = 'button';
+		mute.textContent = 'Mute';
+
+		var volumebar = document.createElement('input');
+		volumebar.id = 'volume-bar';
+		volumebar.className = 'righthand';
+		volumebar.type = 'range';
+		volumebar.min = 0;
+		volumebar.max = 1;
+		volumebar.step = 0.1;
+		volumebar.value = 1;
+
+		var allspan = document.createElement('span');
+		allspan.id = 'video-controls-span';
+		/* mind the order, floats first! */
+		allspan.appendChild(playpause);
+		allspan.appendChild(volumebar);
+		allspan.appendChild(mute);
+		allspan.appendChild(videotime);
+		allspan.appendChild(seekspan);
+
+		var pcontrols = document.createElement('div');
+		pcontrols.id = 'video-controls';
+		pcontrols.appendChild(allspan);
+		pcontrols.allowFullScreen = true;
+
+		var container = document.createElement('div');
+		container.id = 'video-container';
+		container.appendChild(video);
+		container.appendChild(pcontrols);
+
+		/* control callbacks */
+		playpause.addEventListener('click', function() {
+			var video = document.getElementById('avd-html5-video');
+
+			if (video.paused == true) {
+				video.play();
+				playpause.textContent = 'Pause';
+			} else {
+				video.pause();
+				playpause.textContent = 'Play';
+			}
+		});
+
+		mute.addEventListener('click', function() {
+			var video = document.getElementById('avd-html5-video');
+
+			if (video.muted == true)
+				mute.textContent = 'Mute';
+			else
+				mute.textContent = 'Unmute';
+
+			video.muted = !video.muted;
+		});
+
+		seekbar.addEventListener('change', function() {
+			var video = document.getElementById('avd-html5-video');
+
+			video.currentTime = video.duration * (seekbar.value / 100);
+		});
+
+		seekbar.addEventListener('mousedown', function() {
+			var video = document.getElementById('avd-html5-video');
+
+			video.pause();
+		});
+
+		seekbar.addEventListener('mouseup', function() {
+			var video = document.getElementById('avd-html5-video');
+
+			video.play();
+		});
+
+		video.addEventListener('timeupdate', function() {
+			var seekbar = document.getElementById('seek-bar');
+			var videotime = document.getElementById('video-time');
+
+			seekbar.value = (100 / video.duration) * video.currentTime;
+			videotime.textContent = secondsToTimeString(video.currentTime) +
+					' / ' + secondsToTimeString(video.duration);
+		});
+
+		volumebar.addEventListener('change', function() {
+			var video = document.getElementById('avd-html5-video');
+
+			video.volume = volumebar.value;
+		});
+
+		video.addEventListener('click', function (e) {
+			var viddiv = document.getElementById('video-controls');
+
+			if (this.paused || this.ended) {
+				this.play();
+				this.webkitEnterFullscreen();
+			} else {
+				this.pause();
+				this.webkitExitFullscreen();
+			}
+		});
+
+		video.addEventListener('play', function () {
+			if (document.contains(this))
+				this.play();
+			else
+				this.src = '';
+		});
+
+		video.addEventListener('emptied', function() {
+			var seekbar = document.getElementById('seek-bar');
+			var videotime = document.getElementById('video-time');
+			var playpause = document.getElementById('play-pause');
+
+			playpause.textContent = 'Play';
+			seekbar.value = 0;
+			videotime.textContent = secondsToTimeString(0) +
+					' / ' + secondsToTimeString(video.duration);
+		});
+
+		return video;
+	}
+
+	/* CSS insertion */
+	function _insert_css() {
+		var css = document.createElement('style');
+		css.type = 'text/css';
+		css.innerHTML = html5_player_css;
+		document.getElementsByTagName('head')[0].appendChild(css);
 	}
 
 	function _align_widths(arr)
