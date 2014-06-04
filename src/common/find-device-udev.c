3f8b8fc613768ac68e354b7bd432913c4debc0cf
Alban Bedel <alban.bedel@avionic-design.de>
2014-06-04 14:05:45 +0200

    common: Add a generic function to lookup device via udev
    This new API allow to define various match criteria and get a callback
    called for each device found.
    
    Change-Id: I6541f58c274b6d1caa0d4033b0b274edc9d9348d
    Signed-off-by: Alban Bedel <alban.bedel@avionic-design.de>
    Reviewed-on: http://review.adnet.avionic-design.de/1647
    Reviewed-by: Dirk Leber <dirk.leber@avionic-design.de>

--- src/common/find-device-udev.c
+++ src/common/find-device-udev.c
@@ -11,11 +11,10 @@
 #endif
 
 #include <errno.h>
-
+#include <string.h>
 #include <glib.h>
 
 #include <linux/input.h>
-#include <gudev/gudev.h>
 
 #include "find-device.h"
 
@@ -89,6 +88,96 @@ gint find_input_devices(const gchar *devname, device_found_cb callback,
 	g_list_free_full(devices, free_list_item);
 	g_pattern_spec_free(event);
 	g_object_unref(enumerate);
+	g_object_unref(client);
+
+	return found;
+}
+
+gint find_udev_devices(const struct udev_match *match,
+		udev_device_found_cb callback, gpointer user)
+{
+	GUdevEnumerator *enumerator;
+	GList *devices, *node;
+	GUdevClient *client;
+	gint found = 0;
+	int i;
+
+	if (!match)
+		return -EINVAL;
+
+	client = g_udev_client_new(NULL);
+	if (!client) {
+		g_debug("%s: failed to create UDEV client", __func__);
+		return -ENOMEM;
+	}
+
+	enumerator = g_udev_enumerator_new(client);
+	if (!enumerator) {
+		g_debug("%s: failed to create enumerator", __func__);
+		g_object_unref(client);
+		return -ENOMEM;
+	}
+
+	for (i = 0; match->type; i++, match++) {
+		if (!match->value) {
+			g_warning("%s: match %d is missing a value",
+				__func__, i);
+			continue;
+		}
+		if ((match->type & UDEV_MATCH_HAS_KEY) && !match->key) {
+			g_warning("%s: match %d is missing a key",
+				__func__, i);
+			continue;
+		}
+		switch (match->type) {
+		case UDEV_MATCH_SUBSYSTEM:
+			g_udev_enumerator_add_match_subsystem(
+				enumerator, match->value);
+			break;
+		case UDEV_MATCH_NOT_SUBSYSTEM:
+			g_udev_enumerator_add_nomatch_subsystem(
+				enumerator, match->value);
+			break;
+		case UDEV_MATCH_SYSFS_ATTR:
+			g_udev_enumerator_add_match_sysfs_attr(
+				enumerator, match->key, match->value);
+			break;
+		case UDEV_MATCH_NOT_SYSFS_ATTR:
+			g_udev_enumerator_add_nomatch_sysfs_attr(
+				enumerator, match->key, match->value);
+			break;
+		case UDEV_MATCH_PROPERTY:
+			g_udev_enumerator_add_match_property(
+				enumerator, match->key, match->value);
+			break;
+		case UDEV_MATCH_NAME:
+			g_udev_enumerator_add_match_name(
+				enumerator, match->value);
+			break;
+		case UDEV_MATCH_TAG:
+			g_udev_enumerator_add_match_tag(
+				enumerator, match->value);
+			break;
+		default:
+			g_warning("%s: match %d has an unknown type %d",
+				__func__, i, match->type);
+		}
+	}
+
+	devices = g_udev_enumerator_execute(enumerator);
+
+	for (node = g_list_first(devices); node; node = node->next) {
+		GUdevDevice *device = node->data;
+		if (callback) {
+			if (callback(user, device) < 0)
+				callback = NULL;
+		}
+		found += 1;
+		g_object_unref(device);
+	}
+
+	g_list_free(devices);
+	g_object_unref(enumerator);
 	g_object_unref(client);
 
 	return found;
--- src/common/find-device.h
+++ src/common/find-device.h
@@ -8,6 +8,80 @@
 #ifndef __FIND_DEVICE_H__
 #define __FIND_DEVICE_H__
 
+#include <gudev/gudev.h>
+
+/** Define to create match type IDs
+ *
+ * To make the implementation simpler the lower 16 bits of the
+ * ID are used to store a few flags about the type.
+ */
+#define UDEV_MATCH_TYPE(op, flags) (((op) << 16) | (flags))
+
+/** Match type flag for inverted matches */
+#define UDEV_MATCH_NOT		1
+/** Match type flag for invertable matches */
+#define UDEV_MATCH_HAS_NOT	2
+/** Match type flag for types that need a key beside the value */
+#define UDEV_MATCH_HAS_KEY	4
+
+/** Enum of the possible match types */
+enum udev_match_type {
+	/** Match all devices from a subsystem */
+	UDEV_MATCH_SUBSYSTEM = UDEV_MATCH_TYPE(
+		1, UDEV_MATCH_HAS_NOT),
+	/** Match all devices not from a subsystem */
+	UDEV_MATCH_NOT_SUBSYSTEM = UDEV_MATCH_TYPE(
+		1, UDEV_MATCH_HAS_NOT | UDEV_MATCH_NOT),
+	/** Match all devices with a sysfs attribute */
+	UDEV_MATCH_SYSFS_ATTR = UDEV_MATCH_TYPE(
+		2, UDEV_MATCH_HAS_NOT | UDEV_MATCH_HAS_KEY),
+	/** Match all devices without a sysfs attribute */
+	UDEV_MATCH_NOT_SYSFS_ATTR = UDEV_MATCH_TYPE(
+		2, UDEV_MATCH_HAS_NOT | UDEV_MATCH_NOT | UDEV_MATCH_HAS_KEY),
+	/** Match all devices with a udev property */
+	UDEV_MATCH_PROPERTY = UDEV_MATCH_TYPE(
+		3, UDEV_MATCH_HAS_KEY),
+	/** Match all devices with a given kernel name */
+	UDEV_MATCH_NAME = UDEV_MATCH_TYPE(
+		4, 0),
+	/** Match all devices with a given udev tag */
+	UDEV_MATCH_TAG  = UDEV_MATCH_TYPE(
+		5, 0),
+};
+
+/** Struct to hold a matching rule */
+struct udev_match {
+	/** Type of the match */
+	enum udev_match_type type;
+	/** Optional match key, only used for some types */
+	char *key;
+	/** Value to match */
+	char *value;
+};
+
+/**
+ * Callback run when an input device has been found
+ *
+ * @param user The user data passed to the find function
+ * @param name The path of the device found
+ * @return     A negative value to stop calling the callback,
+ *             zero or a positive value to continue
+ */
+typedef int (*udev_device_found_cb)(gpointer user, GUdevDevice *dev);
+
+/**
+ * Lookup devices in udev that match a set of criteria
+ *
+ * @param match    An array of matching critera
+ * @param count    The number of matching critera in the array
+ * @param callback The callback to call when a device has been found
+ * @param user     Userdata pointer to pass to the callback
+ * @return         The number of devices found, otherwise a negative
+ *                 error code
+ */
+gint find_udev_devices(const struct udev_match *match,
+		udev_device_found_cb callback, gpointer user);
+
 typedef int (*device_found_cb)(gpointer user, const gchar *name);
 
 /**
