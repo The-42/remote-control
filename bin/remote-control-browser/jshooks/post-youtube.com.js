a0be657160b87222a3b5c90bb936477571041122
Bert van Hall <bert.vanhall@avionic-design.de>
2014-08-27 16:43:23 +0200

    remote-control-browser: fix youtube play button
    The play button within the html5 youtube player went lost as the
    inserted CSS stopped to work. Fix this by moving the button to its
    parent container and center it by calculating the position based on the
    parent size.
    
    Change-Id: Ie1688b3046f94a65ece92acd9be6fa0fff7a0100
    Signed-off-by: Bert van Hall <bert.vanhall@avionic-design.de>
    Reviewed-on: http://review.adnet.avionic-design.de/1901
    Reviewed-by: Julian Scheel <julian@jusst.de>

--- bin/remote-control-browser/jshooks/post-youtube.com.js
+++ bin/remote-control-browser/jshooks/post-youtube.com.js
@@ -53,6 +53,30 @@
 		document.getElementsByTagName('head')[0].appendChild(css);
 	}
 
+	/* move the play button to the parent container to make overlay work */
+	function move_play_button(ev) {
+		var el = ev.target;
+
+		/* check if we hit the play button at all */
+		if (!el.className || el.className.indexOf('ytp-button-play') < 0)
+			return;
+
+		/* check if it somehow has already been moved */
+		if (el.parentElement.className.indexOf('html5-player-chrome') < 0)
+			return;
+
+		el.parentElement.parentElement.appendChild(el);
+
+		var ytp = document.getElementById('player-api');
+		ytp.removeEventListener('DOMNodeInserted', move_play_button, false);
+	}
+
+	/* the player is loaded a bit later, making this sentinel necessary */
+	function place_button_sentinel() {
+		var ytp = document.getElementById('player-api');
+		ytp.addEventListener('DOMNodeInserted', move_play_button, false);
+	}
+
 	/* Enable html5 video player in favor of flash */
 	function force_html5() {
 		window.ytspf = window.ytspf || {};
@@ -69,6 +93,7 @@
 		insert_css();
 		force_html5();
 		disable_ajax_navigation();
+		place_button_sentinel();
 	}
 
 	init();
