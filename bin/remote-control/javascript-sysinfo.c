2e64445aeb270c3536b803e047083577667a3687
Dirk Leber <dirk.leber@avionic-design.de>
2014-12-04 09:37:36 +0100

    javascript: Add localMacAddress() to Sysinfo
    Returns the mac address of an interface or the first one with an ip v4 address.
    
    Change-Id: I4939893eb210c79fd66078eab7f12cd37d907bc4
    Signed-off-by: Dirk Leber <dirk.leber@avionic-design.de>
    Reviewed-on: http://review.adnet.avionic-design.de/2245
    Reviewed-by: Bert van Hall <bert.vanhall@avionic-design.de>

--- bin/remote-control/javascript-sysinfo.c
+++ bin/remote-control/javascript-sysinfo.c
@@ -16,7 +16,9 @@
 #include <sys/types.h>
 #include <arpa/inet.h>
 #include <net/if.h>
+#include <net/if_arp.h>
 #include <sys/utsname.h>
+#include <sys/ioctl.h>
 
 #include "javascript.h"
 
@@ -100,6 +102,89 @@ cleanup:
 	return ret;
 }
 
+static JSValueRef sysinfo_function_local_mac_address(
+	JSContextRef context, JSObjectRef function, JSObjectRef object,
+	size_t argc, const JSValueRef argv[], JSValueRef *exception)
+{
+	struct sysinfo *inf = JSObjectGetPrivate(object);
+	struct ifaddrs *ifaddr = NULL, *ifa;
+	const unsigned char* mac;
+	char *interface = NULL;
+	JSValueRef ret = NULL;
+	struct ifreq ifr;
+	int fd = -1;
+
+	if (!inf) {
+		javascript_set_exception_text(context, exception,
+				JS_ERR_INVALID_OBJECT_TEXT);
+		goto cleanup;
+	}
+	/* Usage: localMacAddress([interface]) */
+	switch (argc) {
+	case 1:
+		interface = javascript_get_string(context, argv[0], exception);
+		if (!interface)
+			goto cleanup;
+		break;
+	case 0:
+		break;
+	default:
+		javascript_set_exception_text(context, exception,
+				JS_ERR_INVALID_ARG_COUNT);
+		goto cleanup;
+	}
+
+	if (-1 == getifaddrs(&ifaddr)) {
+		javascript_set_exception_text(context, exception,
+			"Failed to get if addresses: %s", g_strerror(errno));
+		goto cleanup;
+	}
+	ifa = sysinfo_get_if(ifaddr, interface);
+	if (!ifa) {
+		javascript_set_exception_text(context, exception,
+			"Failed to get if address");
+		goto cleanup;
+	}
+	if (strlen(ifa->ifa_name) >= sizeof(ifr.ifr_name)) {
+		javascript_set_exception_text(context, exception,
+			"Interface name to long: %s (max %d)", ifa->ifa_name,
+			sizeof(ifr.ifr_name));
+		goto cleanup;
+	}
+	strcpy(ifr.ifr_name, ifa->ifa_name);
+
+	fd = socket(AF_UNIX, SOCK_DGRAM, 0);
+	if (fd == -1) {
+		javascript_set_exception_text(context, exception,
+			"Failed to open socket %d", errno);
+		goto cleanup;
+	}
+	if (ioctl(fd, SIOCGIFHWADDR, &ifr)==-1) {
+		javascript_set_exception_text(context, exception,
+			"Failed to invoke ioctl %d", errno);
+		goto cleanup;
+	}
+	if (ifr.ifr_hwaddr.sa_family != ARPHRD_ETHER) {
+		javascript_set_exception_text(context, exception,
+			"Not an ethernet interface");
+		goto cleanup;
+	}
+	mac = (unsigned char*)ifr.ifr_hwaddr.sa_data;
+	ret = javascript_sprintf(context, exception,
+			"%02X:%02X:%02X:%02X:%02X:%02X",
+			mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+
+cleanup:
+	if (fd != -1)
+		close(fd);
+	if (ifaddr)
+		freeifaddrs(ifaddr);
+	if (interface)
+		g_free(interface);
+
+	return ret;
+}
+
 static JSValueRef sysinfo_function_local_host_name(
 	JSContextRef context, JSObjectRef function, JSObjectRef object,
 	size_t argc, const JSValueRef argv[], JSValueRef *exception)
@@ -158,6 +243,10 @@ static const JSStaticFunction sysinfo_functions[] = {
 	{
 		.name = "localIP",
 		.callAsFunction = sysinfo_function_local_ip,
+		.attributes = kJSPropertyAttributeDontDelete,
+	},{
+		.name = "localMacAddress",
+		.callAsFunction = sysinfo_function_local_mac_address,
 		.attributes = kJSPropertyAttributeDontDelete,
 	},{
 		.name = "localHostName",
