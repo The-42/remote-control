9cd935779fcbe2bf13dd9a7cf01ee4d4fd58005a
Bert van Hall <bert.vanhall@avionic-design.de>
2015-10-12 11:53:51 +0200

    server/watchdog: implement enable-watchdog switch
    Trying to build remote-control without --enable-watchdog failed due to a
    hardcoded watchdog dependency. Implement this configure switch for
    app-watchdog.
    
    Change-Id: Ib5246499ec2f4092e030c5ff1beceb50aa8d3b3f
    Signed-off-by: Bert van Hall <bert.vanhall@avionic-design.de>
    Reviewed-on: http://review.adnet.avionic-design.de/4300
    Reviewed-by: Dirk Leber <dirk.leber@avionic-design.de>

---
+++ src/server/app-watchdog-null.c
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2015 Avionic Design GmbH
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif
+
+#include <errno.h>
+
+#include "remote-control.h"
+
+struct app_watchdog {
+	void *dummy;
+};
+
+int app_watchdog_start(struct app_watchdog *watchdog, int interval)
+{
+	return -ENOSYS;
+}
+
+int app_watchdog_stop(struct app_watchdog *watchdog)
+{
+	return -ENOSYS;
+}
+
+int app_watchdog_trigger(struct app_watchdog *watchdog)
+{
+	return -ENOSYS;
+}
+
+int app_watchdog_create(struct app_watchdog **watchdogp, GKeyFile *config)
+{
+	return 0;
+}
